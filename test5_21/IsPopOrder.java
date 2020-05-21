2ec032a0cae208a13a496e60240849469967b93c
try-max <qijiale_1128@163.com>
2020-05-21 18:32:15 +0800

    判断该栈的弹出顺序是否正确

---
+++ test5_21/IsPopOrder.java
@@ -0,0 +1,38 @@
+package test5_20;
+
+/**判断该栈的弹出顺序是否正确
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
+ * 假设压入栈的所有数字均不相等。
+ * 例如序列1,2,3,4,5是某栈的压入顺序，
+ * 序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+ * （注意：这两个序列的长度是相等的）
+ * @author Q
+ * @create 2020-05-20-15:21
+ */
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class IsPopOrder {
+    public static boolean isPopOrder(int [] pushA,int [] popA) {
+        if (popA.length==0||pushA.length==0||popA.length!=pushA.length){
+            return false;
+        }
+        Stack<Integer> stack=new Stack<>();
+        int j=0;
+        for (int i = 0; i <pushA.length; i++) {
+            stack.push(pushA[i]);
+
+            while (!stack.isEmpty()&&stack.peek()==popA[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        int[] pushA=new int[]{1,2,3,4,5};
+        int[] popA=new int[]{4,5,3,2,1};
+        System.out.println(isPopOrder(pushA, popA));
+    }
+}
